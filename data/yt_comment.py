97e647df7fd5ceff2ccc4910dc973600d023d2a6
Prabesh Shrestha <prabeshshrestha90@gmail.com>
2024-09-14 09:52:42 +0545

    max 100 comments

--- data/yt_comment.py
+++ data/yt_comment.py
@@ -14,7 +14,7 @@ def get_comments(video_id):
     request = youtube.commentThreads().list(
         part="snippet",
         videoId=video_id,
-        maxResults=1000
+        maxResults=100
     )
     
     response = request.execute()
